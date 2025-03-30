0cffb24e246e4872fd8754c87ffeff134b08ab59
aman <amankumarchy5423@gmail.com>
2025-03-30 10:15:46 +0530

    file commited

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -4,7 +4,11 @@ import os
 from datetime import datetime
 
 
-LOG_FILE =f"{datetime.now().strftime("%Y_%m_%d_%H_%M_%S.log")}.log" 
+# LOG_FILE =f"{datetime.now().strftime("%Y_%m_%d_%H_%M_%S.log")}.log" 
+# LOG_FILE =f"{datetime.now().strftime("%Y_%m_%d_%H_%M_%S.log")}.log"
+LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+
+
 
 LOG_DIR = os.path.join(os.getcwd(), "logs",LOG_FILE)
 os.makedirs(LOG_DIR, exist_ok=True)  
